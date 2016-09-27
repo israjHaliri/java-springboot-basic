9668534f812a57c196d0c147ed47e9281716e575
israjhaliri <israj.haliri@gmail.com>
2016-09-27 15:25:49 +0700

    change pacakge to war

--- src/main/java/com/mywork/springboot1/Springboot1Application.java
+++ src/main/java/com/mywork/springboot1/Springboot1Application.java
@@ -3,14 +3,20 @@ package com.mywork.springboot1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class Springboot1Application {
+public class Springboot1Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot1Application.class, args);
+	}
+
+	public SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Springboot1Application.class);
 	}
 }
