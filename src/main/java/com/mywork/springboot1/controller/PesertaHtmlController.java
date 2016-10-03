d2b100b4e9950b35e5cf166332d901cc67b6b181
israjhaliri <israj.haliri@gmail.com>
2016-10-03 15:42:08 +0700

    add upload file

--- src/main/java/com/mywork/springboot1/controller/PesertaHtmlController.java
+++ src/main/java/com/mywork/springboot1/controller/PesertaHtmlController.java
@@ -9,8 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -47,20 +50,28 @@ public class PesertaHtmlController {
     }
 
     @RequestMapping( value = "/form", method = RequestMethod.GET)
-    public String tampilForm(@RequestParam("id") String id,Model m){
+    public String tampilForm(@RequestParam(value = "id",required = false) String id,Model m){
 
-        if (id !=null || !id.isEmpty()){
+        m.addAttribute("peserta",new Peserta());
+        if (id !=null && !id.isEmpty()){
             Peserta p = pd.findOne(id);
             if (p !=null){
                 m.addAttribute("peserta",p);
             }
-        }
+        }else{
 
+        }
         return "/peserta/form";
+
     }
 
     @RequestMapping( value = "/form", method = RequestMethod.POST)
-    public String prosesForm(@Valid Peserta peserta, BindingResult bindingResult){
+    public String prosesForm(
+            @Valid Peserta peserta,
+            BindingResult bindingResult,
+            MultipartFile foto,
+            HttpSession session) throws Exception
+    {
         if (bindingResult.hasErrors()){
             return "/peserta/form";
         }
@@ -71,6 +82,37 @@ public class PesertaHtmlController {
         System.out.println(peserta.getTanggalLahir());
         System.out.println("============================");
         pd.save(peserta);
+
+        String namaFile = foto.getName();
+        String jenisFile = foto.getContentType();
+        String namaAsli = foto.getOriginalFilename();
+        Long ukuran = foto.getSize();
+
+        System.out.println("===========================");
+        System.out.println(namaFile);
+        System.out.println(jenisFile);
+        System.out.println(namaAsli);
+        System.out.println(ukuran);
+        System.out.println("===========================");
+
+        String lokasiPath = "/upload";
+        String lokasiTomcat = session.getServletContext().getRealPath(lokasiPath);
+        System.out.println("lokasi tomcat di jalankan :"+lokasiTomcat);
+//        String lokasiTujuan = lokasiTomcat + File.separator;
+
+//        File folderTujuan =  new File(lokasiTomcat + File.separator);
+        String homeFolder = System.getProperty("user.home");
+        String lokasiTujuan = homeFolder + File.separator + "tmp";
+        File folderTujuan =  new File(lokasiTujuan);
+        if (!folderTujuan.exists()){
+            folderTujuan.mkdirs();
+        }
+
+        File tujuan = new File(lokasiTujuan + File.separator + namaAsli);
+
+        foto.transferTo(tujuan);
+        System.out.println("file sudah di copy ke : "+tujuan.getAbsolutePath());
+
         return "redirect:list";
     }
 
